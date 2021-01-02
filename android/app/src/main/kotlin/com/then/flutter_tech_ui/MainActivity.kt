3b633794a16f27209c8dbc08040d6c12a17da48d
Naing Myo Thaw <naingmyothaw.nmyt@gmail.com>
2021-01-02 20:58:55 +0630

    first commit

---
+++ android/app/src/main/kotlin/com/then/flutter_tech_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.then.flutter_tech_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tech UI',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Tech UI'),
+      ),
+    );
+  }
+}
